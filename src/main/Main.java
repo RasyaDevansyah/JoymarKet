6b264709b8d9e9ae449913a5ce799bba6deba16f
RasyaDevansyah <rasya.devan21@gmail.com>
2025-11-27 15:15:15 +0700

    Initialize libraries

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+	public Main() {
+		
+		System.out.println("Hello World");
+		
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
